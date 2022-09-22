7f7bc80c67c26fe948ba78fb83d130f72aa3c996
sho-s-git <rikotai28@icloud.com>
2022-09-23 01:47:44 +0900

    new expo camera

--- src/screens/HandoutRegister.jsx
+++ src/screens/HandoutRegister.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet, Text, TouchableOpacity, View,
+} from 'react-native';
 import Constants from 'expo-constants';
 
 function HandoutRegister() {
+  const [type, setType] = useState(CameraType.back);
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  function toggleCameraType() {
+    setType(
+      type === CameraType.back
+        ? CameraType.front
+        : CameraType.back,
+    );
+  }
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>sample</Text>
+      <Camera style={{ flex: 1 }} type={type}>
+        <View>
+          <TouchableOpacity onPress={toggleCameraType}>
+            <Text>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
